b036647e19189a18ec511e17b96fe4a4729c5be7
John Laffey <laff@pop-os.localdomain>
2023-01-08 10:33:56 -0500

    Uploading to github

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from config import APP_KEY
+from .config import APP_KEY
 import os
 
 
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = APP_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
